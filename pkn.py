6bcfac616b9257a2d636e84d0d1c72faab6388d6
Fivlas <81146191+Fivlas@users.noreply.github.com>
2022-11-03 21:11:57 +0100

    Update pkn.py

--- pkn.py
+++ pkn.py
@@ -1,46 +1,49 @@
 import random
 
+opcje = ["papier", "kamien", "nozyce"]
+random1 = random.randint(0,2)
+bot = opcje[random1]
 user_wins = 0
 bot_wins = 0
 game_draw = 0
 
-options = ["rock", "paper", "scissors"]
-options[0]
 while True:
-    user_input = input("Type Rock/Papier/Scissors or Q to quit: ").lower()
-    if user_input == "q":
-        break
-    
-    if user_input not in options:
-        print("Invalid input")
+    player_pick = input("Wybierz Papier/Kamien/Nozyce lub quit by wyjsc lub wyniki by sprawdzic wyniki\n").lower()
+
+    if player_pick == "quit":
+        cos = input("jestes pewny Y/n\n").lower()
+        if(cos == "y"):
+            break
+        else:
+            continue
+
+    if player_pick == "wyniki":
+        print("\nWygrales:", user_wins, "\nBot wygrał:", bot_wins, "\nRemis:", game_draw)
         continue
-    
-    random_number = random.randint(0, 2)
-    # rock: 0, paper: 1, scissors: 2 
-    bot_pick = options[random_number]
-    print("Bot picked", bot_pick + ".")
-    
-    if user_input == "rock" and bot_pick == "scissors":
-        print("You Won!")
+
+
+    if player_pick not in opcje and not "wyniki":
+        print("\nNie ma takiego ruchu")
+        continue
+
+    print("\nBot wybral", bot + ".")
+
+    if player_pick == "kamien" and bot == "nozyce":
+        print("Wygrales!\n")
         user_wins += 1
     
-    elif user_input == "paper" and bot_pick == "rock":
-        print("You Won!")
+    elif player_pick == "papier" and bot == "kamien":
+        print("Wygrales!\n")
         user_wins += 1
     
-    elif user_input == "scissors" and bot_pick == "paper":
-        print("You Won!")
+    elif player_pick == "nozyce" and bot == "papier":
+        print("Wygrales!\n")
         user_wins += 1
     
-    elif user_input == bot_pick:
-        print("Draw!")
+    elif player_pick == bot:
+        print("Remis!\n")
         game_draw += 1
         
     else:
-        print("You Lost!")
+        print("Przegrales!\n")
         bot_wins += 1
-        
-print("You won", user_wins, "times.")
-print("The bot won", bot_wins, "times." )
-print("Draw was", game_draw, "times.")
-print("Goodbye!")
